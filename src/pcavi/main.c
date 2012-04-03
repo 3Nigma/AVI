13028009a9c95a3e5844301c30bb1b9f7afdcbe4
Victor ADASCALITEI <adascalitei.victor@gmail.com>
2012-04-03 08:18:43 +0300

    Cleaned up the formatting of the 'pcavi' app.

--- src/pcavi/main.c
+++ src/pcavi/main.c
@@ -39,74 +39,26 @@
 #define RX_LED PD5
 #define TX_LED PD6
 
-int main(void)
-{
-	uint8_t msg[] = "Test";
+int main(void) {
+  uint8_t msg[] = "Test";
 	
-	uart_init();
-	uart_putstr ("Hello");
-	
-	DDRD |= _BV(RX_LED) | _BV(TX_LED);
-	
-	/* set nRES pin of RFM12 HIGH */
-	DDRD |= _BV(PD3);
-	PORTD &= ~_BV(PD3);
-	_delay_ms(10);
-	PORTD |= _BV(PD3);
-	_delay_ms(100);
-	
-	initRF();
-	
-    while(1)
-    {
-		rf12_txdata(msg, sizeof(msg));
-		PORTD ^= _BV(TX_LED);	
-		_delay_ms(1000);	
-	}
+  uart_init();
+  uart_putstr ("Hello");
+  
+  DDRD |= _BV(RX_LED) | _BV(TX_LED);
+  
+  /* set nRES pin of RFM12 HIGH */
+  DDRD |= _BV(PD3);
+  PORTD &= ~_BV(PD3);
+  _delay_ms(10);
+  PORTD |= _BV(PD3);
+  _delay_ms(100);
+  
+  initRF();
+  
+  while(1) {
+      rf12_txdata(msg, sizeof(msg));
+      PORTD ^= _BV(TX_LED);	
+      _delay_ms(1000);	
+  }
 }
-
-/*
-#include <avr/io.h>
-#include <avr/interrupt.h>
-#include <avr/pgmspace.h>
-#include <avr/eeprom.h>
-#include <stdlib.h>
-#include "global.h"
-#include "rf12.h"     
-
-#define F_CPU 10000000UL
-#include <util/delay.h>
-
-void send(void);
-void receive(void);
-
-int main(void)
-{
-	rf12_init();					// ein paar Register setzen (z.B. CLK auf 10MHz)
-	rf12_setfreq(RF12FREQ(433.92));	// Sende/Empfangsfrequenz auf 433,92MHz einstellen
-	rf12_setbandwidth(4, 1, 4);		// 200kHz Bandbreite, -6dB Verstärkung, DRSSI threshold: -79dBm 
-	rf12_setbaud(19200);			// 19200 baud
-	rf12_setpower(0, 6);			// 1mW Ausgangangsleistung, 120kHz Frequenzshift
-
-	for (;;)
-	{
-		send();
-		for (unsigned char i=0; i<100; i++)
-			_delay_ms(10);
-
-	//	receive();
-	}
-}
-
-void receive(void)
-{	unsigned char test[32];	
-	rf12_rxdata(test,32);	
-	// daten verarbeiten
-}
-
-void send(void)
-{	unsigned char test[]="Dies ist ein 433MHz Test !!!\n   ";	
-	rf12_txdata(test,32);
-}
-
-*/
--- src/pcavi/rf12.c
+++ src/pcavi/rf12.c
@@ -30,162 +30,136 @@
 
 #include "rf12.h"
 
-uint16_t rf12_wrt_cmd(uint16_t cmd)
-{
-	uint8_t cnt;
-	uint16_t toRet = 0;
-	
-	RF_PORT &= ~_BV(SCK);
-	RF_PORT &= ~_BV(CS);
-	
-	for(cnt = 0; cnt < 16; ++ cnt)
-	{
-		if(cmd & 0x8000) RF_PORT |= _BV(SDI);
-		else RF_PORT &= ~_BV(SDI);
-		RF_PORT |= _BV(SCK);
-		toRet <<= 1;
-		if(RF_PIN & _BV(SDO)) toRet |= 0x0001;
-		cmd <<= 1;
-		RF_PORT &= ~_BV(SCK);
-	}
-	
-	RF_PORT &= ~_BV(SCK);
-	RF_PORT |= _BV(CS);
-	
-	return toRet;
+uint16_t rf12_wrt_cmd(uint16_t cmd) {
+  uint8_t cnt;
+  uint16_t toRet = 0;
+  
+  RF_PORT &= ~_BV(SCK);
+  RF_PORT &= ~_BV(CS);
+  
+  for(cnt = 0; cnt < 16; ++ cnt){
+    if(cmd & 0x8000) RF_PORT |= _BV(SDI);
+    else RF_PORT &= ~_BV(SDI);
+    RF_PORT |= _BV(SCK);
+    toRet <<= 1;
+    if(RF_PIN & _BV(SDO)) toRet |= 0x0001;
+    cmd <<= 1;
+    RF_PORT &= ~_BV(SCK);
+  }
+  
+  RF_PORT &= ~_BV(SCK);
+  RF_PORT |= _BV(CS);
+  
+  return toRet;
 }
 
-void initRF(void)
-{
-	/* initialize rf port connections */
-	RF_PORT |= _BV(CS) | _BV(SDI);
-	RF_PORT &= ~_BV(SCK);
-	RF_DDR |=_BV(SDI) | _BV(SCK) | _BV(CS);
-	RF_DDR &= ~_BV(SDO);
-	RF_INT_DDR &= ~_BV(NIRQ);
+void initRF(void) {
+  /* initialize rf port connections */
+  RF_PORT |= _BV(CS) | _BV(SDI);
+  RF_PORT &= ~_BV(SCK);
+  RF_DDR |=_BV(SDI) | _BV(SCK) | _BV(CS);
+  RF_DDR &= ~_BV(SDO);
+  RF_INT_DDR &= ~_BV(NIRQ);
+  
+  _delay_ms(100);			/* wait until POR done */
+  
+  rf12_wrt_cmd(0x0000);	 /* fix nIRQ generating bug */  
+  rf12_wrt_cmd(0x80E7);  /* EL,EF,12.0pF */
+  rf12_wrt_cmd(0xA640);  /* 868MHz */
+  rf12_wrt_cmd(0xC647);  /* 4.8Kbps (38.4: 8, 19.2: 11, 9.6: 23, 4.8: 47) */
+  rf12_wrt_cmd(0x94A0);  /* VDI,FAST,134kHz,0dBm,-103dBm */
+  rf12_wrt_cmd(0xC2AC);  /* AL,!ml,DIG,DQD4 */
+  rf12_wrt_cmd(0xCA83);  /* FIFO8,SYNC,!ff,DR */
+  rf12_wrt_cmd(0xCED4);  /* SYNC=2DXX? */
+  rf12_wrt_cmd(0xC483);  /* @PWR,NO RSTRIC,!st,!fi,OE,EN */
+  rf12_wrt_cmd(0x9850);  /* !mp,90kHz,MAX OUT */ 
+  rf12_wrt_cmd(0xCC77);  /* OB1?OB0, LPX,?ddy?DDIT?BW0 */
+  rf12_wrt_cmd(0xE000);  /* NOT USE */
+  rf12_wrt_cmd(0xC800);  /* NOT USE */
+  rf12_wrt_cmd(0xC040);  /* 1.66MHz,2.2V */
+  rf12_wrt_cmd(0x8209);  /* enable xtal, disable clk pin */
 	
-	_delay_ms(100);			/* wait until POR done */
-
-	rf12_wrt_cmd(0x0000);	/* fix nIRQ generating bug */
-	//rf12_wrt_cmd(0x80E7);   /* EL,EF,868band,12.0pF */ 
-	//rf12_wrt_cmd(0xABB8);   /* 875MHz */
-	//rf12_wrt_cmd(0xC603);   /* 86.2kbps */ 
-	//rf12_wrt_cmd(0x94A0);   /* VDI,FAST,134kHz,0dBm,-103dBm */ 
-	//rf12_wrt_cmd(0xC2AC);   /* AL,!ml,DIG,DQD4 */ 
-	//rf12_wrt_cmd(0xCA81);   /* FIFO8,SYNC,!ff,DR */ 
-	//rf12_wrt_cmd(0xC483);   /* @PWR,NO RSTRIC,!st,!fi,OE,EN */ 
-	//rf12_wrt_cmd(0x9850);   /* !mp,9810=30kHz,MAX OUT */
-	//rf12_wrt_cmd(0xC800);   /* Low duty-cycle : NOT USED */ 
-	//rf12_wrt_cmd(0xC0E0);   /* 10MHz, 2.2V */ 
-	//rf12_wrt_cmd(0xE000);	/* wakeup timer : NOT USED */ 
-	//rf12_wrt_cmd(0x8209);   /*  */
-	
-	rf12_wrt_cmd(0x80E7); // EL,EF,12.0pF
-    rf12_wrt_cmd(0xA640); // 868MHz
-    rf12_wrt_cmd(0xC647); // 4.8Kbps (38.4: 8, 19.2: 11, 9.6: 23, 4.8: 47)
-    rf12_wrt_cmd(0x94A0); // VDI,FAST,134kHz,0dBm,-103dBm
-    rf12_wrt_cmd(0xC2AC); // AL,!ml,DIG,DQD4
-    rf12_wrt_cmd(0xCA83); // FIFO8,SYNC,!ff,DR
-    rf12_wrt_cmd(0xCED4); // SYNC=2DXX?
-    rf12_wrt_cmd(0xC483); // @PWR,NO RSTRIC,!st,!fi,OE,EN
-    rf12_wrt_cmd(0x9850); // !mp,90kHz,MAX OUT
-    rf12_wrt_cmd(0xCC77); // OB1?OB0, LPX,?ddy?DDIT?BW0
-    rf12_wrt_cmd(0xE000); // NOT USE
-    rf12_wrt_cmd(0xC800); // NOT USE
-    rf12_wrt_cmd(0xC040); // 1.66MHz,2.2V
-	rf12_wrt_cmd(0x8209); // enable xtal, disable clk pin
-	
-	/* configure RF device interrupt */
-	//MCUCR |= _BV(ISC01);		/* raise an interrupt on falling edge of INT0 */
-	//MCUCR &= ~_BV(ISC00);
-	//GIMSK |= _BV(INT0);			/* enable external interrupt on INT0 */
+  /* configure RF device interrupt */
+  //MCUCR |= _BV(ISC01);  /* raise an interrupt on falling edge of INT0 */
+  //MCUCR &= ~_BV(ISC00);
+  //GIMSK |= _BV(INT0);  /* enable external interrupt on INT0 */
 }
 
-ISR(INT0_vect)
-{
-	uint16_t rfStatus = rf12_wrt_cmd(0x0000);	/* read the status of the RF device */
-	uint16_t recvDblChr;
+ISR(INT0_vect) {
+  uint16_t rfStatus = rf12_wrt_cmd(0x0000);  /* read the status of the RF device */
+  uint16_t recvDblChr;
 	
-	if(rfStatus & 0x1000)
-	{
-		rf12_wrt_cmd(0x8201);
-		rf12_wrt_cmd(0x8203);
-	}
-	if(rfStatus & 0x8000)	/* TX register ready to receive the next byte OR
-							   //RX FIFO reached pre-programmed limit */
-	{
-		PORTD ^= _BV(PD6);
-		rf12_wrt_cmd(0xB800 + 0x55);
-	}
+  if(rfStatus & 0x1000) {
+    rf12_wrt_cmd(0x8201);
+    rf12_wrt_cmd(0x8203);
+  }
+  if(rfStatus & 0x8000)	{  /* TX register ready to receive the next byte OR receiver got to the preprogrammed byte count */
+    PORTD ^= _BV(PD6);
+    rf12_wrt_cmd(0xB800 + 0x55);
+  }
 }
 
-void rf12_send_char(unsigned char toSend)
-{
-	while(RF_INT_PIN & _BV(NIRQ));	/* wait for previous TX to finish */
-	rf12_wrt_cmd(0xB800 + toSend);
+void rf12_send_char(unsigned char toSend) {
+  while(RF_INT_PIN & _BV(NIRQ));  /* wait for previous TX to finish */
+  rf12_wrt_cmd(0xB800 + toSend);
 }
 
-uint8_t rf12_recv_char(void)
-{
-	uint16_t recvDblChr;
-	
-	while(RF_INT_PIN & _BV(NIRQ));
-	rf12_wrt_cmd(0x0000);
-	recvDblChr = rf12_wrt_cmd(0xB000);
-	return (recvDblChr & 0x00FF);
+uint8_t rf12_recv_char(void) {
+  uint16_t recvDblChr;
+  
+  while(RF_INT_PIN & _BV(NIRQ));
+  rf12_wrt_cmd(0x0000);
+  recvDblChr = rf12_wrt_cmd(0xB000);
+  return (recvDblChr & 0x00FF);
 }
 
-void rf12_txdata(uint8_t *data, uint8_t length)
-{		
-	uint8_t cnt;
-	uint8_t checksum = 0;
+void rf12_txdata(uint8_t *data, uint8_t length) {		
+  uint8_t cnt;
+  uint8_t checksum = 0;
 	
-	rf12_wrt_cmd(0x0000);		/* read status register */
-	//rf12_wrt_cmd(0xB8AA);
-	rf12_wrt_cmd(0xB8AA);
-	rf12_wrt_cmd(0x8239);   /* !ER, !EBB, ET, ES, EX, !EB, EW, DC */
-	/* send preamble */
-	rf12_send_char(0xAA);
-	rf12_send_char(0xAA);
-	rf12_send_char(0x2D);		/* send SYNC High byte */
-	rf12_send_char(0xD4);		/* send SYNC Low byte */
+  rf12_wrt_cmd(0x0000);  /* read status register */
+  //rf12_wrt_cmd(0xB8AA);
+  rf12_wrt_cmd(0xB8AA);
+  rf12_wrt_cmd(0x8239);  /* !ER, !EBB, ET, ES, EX, !EB, EW, DC */
+  /* send preamble */
+  rf12_send_char(0xAA);
+  rf12_send_char(0xAA);
+  rf12_send_char(0x2D);  /* send SYNC High byte */
+  rf12_send_char(0xD4);  /* send SYNC Low byte */
 	
-	/* send useful data */
-	rf12_send_char('G');		/* send data length byte */
-	rf12_send_char('A');
-	rf12_send_char('G');
-	rf12_send_char('U');
-	rf12_send_char('T');
-	rf12_send_char('A');
-	rf12_send_char('T');
-	rf12_send_char('I');
-	/*for(cnt = 0; cnt < length; ++ cnt)
-	{
-		rf12_send_char(data[cnt]);
-		checksum += data[cnt];
-	}*/
-	//rf12_send_char(checksum);	/* send raw checksum byte */
+  /* send useful data */
+  rf12_send_char('G');  /* send data length byte */
+  rf12_send_char('A');
+  rf12_send_char('G');
+  rf12_send_char('U');
+  rf12_send_char('T');
+  rf12_send_char('A');
+  rf12_send_char('T');
+  rf12_send_char('I');
+  /*for(cnt = 0; cnt < length; ++ cnt) {
+    rf12_send_char(data[cnt]);
+    checksum += data[cnt];
+    }*/
+  //rf12_send_char(checksum);  /* send raw checksum byte */
 	
-	/* finish TX session */
-	rf12_send_char(0xAA);
-	rf12_send_char(0xAA);
-	rf12_wrt_cmd(0x8209);   /* !ER, !EBB, ET, ES, EX, !EB, EW, DC */
+  /* finish TX session */
+  rf12_send_char(0xAA);
+  rf12_send_char(0xAA);
+  rf12_wrt_cmd(0x8209);  /* !ER, !EBB, ET, ES, EX, !EB, EW, DC */
 	
-	_delay_ms(10);
+  _delay_ms(10);
 }
 
-void rf12_rxdata(uint8_t *data, uint8_t length)
-{	
-	uint8_t i;
+void rf12_rxdata(uint8_t *data, uint8_t length)  {	
+  uint8_t i;
 	
-	rf12_wrt_cmd(0x82C9);				// RX on
-	rf12_wrt_cmd(0xCA81);				// set FIFO mode
-	rf12_wrt_cmd(0xCA83);				// enable FIFO
-	for (i = 0; i < length; ++i)
-	{	
-		*data++ = rf12_recv_char();
-	}
-	
-	rf12_wrt_cmd(0x8209);				// RX off
+  rf12_wrt_cmd(0x82C9);  /* RX on */
+  rf12_wrt_cmd(0xCA81);  /* set FIFO mode */
+  rf12_wrt_cmd(0xCA83);	 /* enable FIFO */
+  for (i = 0; i < length; ++i) {	
+    *data++ = rf12_recv_char();
+  }
+  
+  rf12_wrt_cmd(0x8209);  /* RX off */
 }
 
